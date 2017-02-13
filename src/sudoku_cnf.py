26e75e448649edf09f460213643d4ed159d0e7b4
Jona Scott <Jona Scott>
2017-02-12 21:29:53 -0500

    CNF encoder finished. Needs to be confirmed.

--- src/sudoku_cnf.py
+++ src/sudoku_cnf.py
@@ -26,7 +26,9 @@ def cells(size,m):
                 cell_file.write(str(value) + " ")
                 line = line + str(value) + " "
             cell_file.write("0\n")
+            out_file.write(line + "0\n")
             write_negations(negate(line), cell_file)
+            write_negations(negate(line), out_file)
             
 def rows(size,m):
     size=int(size)+1
@@ -38,7 +40,9 @@ def rows(size,m):
                 row_file.write(str(value) + " ")
                 line = line + str(value) + " "
             row_file.write("0\n")
+            out_file.write(line + "0\n")
             write_negations(negate(line), row_file)
+            write_negations(negate(line), out_file)
 
 def columns(size,m):
     size=int(size)+1
@@ -50,18 +54,52 @@ def columns(size,m):
                 column_file.write(str(value) + " ")
                 line = line + str(value) + " "
             column_file.write("0\n")
+            out_file.write(line + "0\n")
             write_negations(negate(line), column_file)
+            write_negations(negate(line), out_file)
             
+def block(size, m):
+    s = int(size) + 1
+    step = int(math.sqrt(int(size)))
+    first = 1
+    first_temp=first
+    mid = 1
+    mid_temp=mid
+    last = 1
+    for b in range(1, s, step):
+        for a in range(1, s, step):
+            for i in range(1, s):
+                block_string=''
+                for k in range(1,step+1):
+                    for l in range(1,step+1):
+                        value = m**2*first + m*mid + last
+                        block_string = block_string + str(value) + " " 
+                        mid += 1
+                    mid = mid_temp
+                    first += 1
+                last+= 1
+                block_file.write(block_string + "0\n")
+                write_negations(negate(block_string), block_file)
+                write_negations(negate(block_string), out_file)
+                first= first_temp
+            last = 1
+            mid += step
+            mid_temp = mid
+        first += step
+        first_temp += step
+        mid = 1
+        mid_temp = mid
+
 def write_negations(neg_list, file):
     for i in neg_list:
         clause = ''
         for j in i:
             clause = clause + j + " "
-        print(clause)
+        #print(clause)
         file.write(clause + "0\n")
 
 def negate(clause):
-    print(clause)
+    #print(clause)
     literals = clause.split(" ")[:-1]
     neg_lit = []
     negation = "-"
@@ -76,8 +114,9 @@ def main(argv):
         m = 17
     else:
         m = 10
+    cells(argv[0], m)
+    block(argv[0],m)
     rows(argv[0], m)
     columns(argv[0], m)
-    cells(argv[0], m)
 if __name__ == "__main__":
     main(sys.argv[1:])
