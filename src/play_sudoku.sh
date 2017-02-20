6869456b49f75d8eaeec4247c0f40f99f4378b1a
Jona Scott <Jona Scott>
2017-02-19 23:39:04 -0500

    updated with checking parameter and correct init var.

--- src/play_sudoku.sh
+++ src/play_sudoku.sh
@@ -4,6 +4,7 @@ TRIALS=$1
 BSIZE=$2
 START_INIT=$3
 END_INIT=$4
+CHECK=$5
 counter=$3
 rm cnf_files/fstage/frun/*
 rm results/*
@@ -12,17 +13,17 @@ do
 	for trial in $(seq 1 $TRIALS)
 	do
 		touch temp.txt
-		python initialize_board.py "$BSIZE" "$counter">temp.txt
-		actual_init=$(wc -l temp.txt)
+		python initialize_board.py "$BSIZE" "$counter" "$CHECK" >temp.txt
+		actual_init=$(wc -l < temp.txt)
 		echo "tried: $counter -> got: $actual_init"
-		cp cnf_files/fstage/fclean/problem_"$2"_"$counter".txt cnf_files/fstage/frun/problem_"$2"_"$actual_init"_"$trial".cnf 
+		cp cnf_files/fstage/fclean/problem_"$2"_"$actual_init".txt cnf_files/fstage/frun/problem_"$2"_"$actual_init"_"$trial".cnf 
 		#echo $'\r' >> cnf_files/fstage/frun/problem_"$2"_"$counter"_"$trial".txt
 		while read line; do
-			echo "$line" >> cnf_files/fstage/frun/problem_"$2"_"$counter"_"$trial".cnf
+			echo "$line" >> cnf_files/fstage/frun/problem_"$2"_"$actual_init"_"$trial".cnf
 		done <temp.txt
 		rm temp.txt
-		minisat cnf_files/fstage/frun/problem_"$2"_"$counter"_"$trial".cnf > results/r"$BSIZE"_"$counter"_"$TRIALS"_"$trial".sol
-		echo r"$BSIZE"_"$counter"_"$TRIALS"_"$trial".sol
+		minisat cnf_files/fstage/frun/problem_"$2"_"$actual_init"_"$trial".cnf > results/r"$BSIZE"_"$actual_init"_"$TRIALS"_"$trial".sol
+		echo r"$BSIZE"_"$actual_init"_"$TRIALS"_"$trial".sol
 	done
 	let counter+=1
 done
