80e03d4fe8accb27aa452df26f87f1de5453b17f
Jona Scott <Jona Scott>
2017-02-22 19:12:30 +0000

    example chart of sat.

--- src/data/summary_results/plot_phase.sh
+++ src/data/summary_results/plot_phase.sh
@@ -8,8 +8,8 @@ set style line 2  lc rgb '#0025ad' lt 1 lw 1.5 # --- blue
 set style line 3 lc rgb '#09ad00' lt 1 lw 1.5 # --- green
 set style line 4 lc rgb "red" lt 1 lw 1.5 # --- green
 set term png
-set output '../../plots/sat_class.png'
-set title 'Class Data: %SAT vs Ratio'
+set output 'plots/sat_class.png'
+set title '%SAT vs Ratio'
 set xlabel 'Ratio'
 set ylabel '% SAT'
 set grid
@@ -17,32 +17,5 @@ set key below
 #############
 # SAT Graph #
 #############
-#get a list of all files in directory
-file_list=system('ls -1B *.dat')
-set yrange[0:1.1]
-#loop through and plot all files in directory fpr %sat-ratio
-plot for [file in file_list] file u 3:4 smooth csplines with lines t file
-clear
-#############
-# Dec Graph #
-#############
-set title 'Class Data: Number of Decisions vs Ratio'
-set ylabel 'Number of Decisions'
-set output '../../plots/dec_class.png'
-set yrange[0:850000]
-#loop through and plot all files in directory for numdec-ratio
-plot for [file in file_list] file u 3:5 smooth csplines with lines t file
-clear
-#####################
-# SAT and Dec Graph #
-#####################
-#set grid x y2
-#set y2range[0:1]
-#set y2label "Percent SAT"
-#set y2tics .1 nomirror 2
-#set title 'Number of Decisions and Percent SAT vs Ratio'
-#set ylabel 'Number of Decisions'
-#set output '../plots/sat_desc.png'
-#loop through and overlay sat and desc plots
-#plot for [file in file_list] file u 1:3 smooth csplines with lines t file, for [file in file_list] file u 1:2 axes x1y2 smooth csplines with lines t file
+plot "summary_for_size_.txt" using 1:2 title "2 Trials" with lines
 EOF
