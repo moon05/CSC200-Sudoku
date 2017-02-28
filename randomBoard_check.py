5f0766a8110aff0d973be6e5f0e0acfa0f9d4b39
Abdullah Al Mamun <Moon@Abdullahs-MacBook-Pro.local>
2017-02-27 22:06:48 -0500

    added 2 pngs for sudoku

--- randomBoard_check.py
+++ randomBoard_check.py
@@ -9,8 +9,11 @@ def boardGen(size, inVar):
 	# 2nd random to locate the small cell
 	# 3rd random to fill in the number
 
+	
+	# 
+
 	#checks for proper square
-	if size is not 4 or 9 or 16:
+	if size not in [4,9,16]:
 		print "Not proper square. Please enter 4, 9 or 16"
 		return
 	a = isinstance( inVar, (int, long) )
@@ -23,11 +26,18 @@ def boardGen(size, inVar):
 		return
 
 	# create list, as it maintains order, to hold all rows in CNF form
-	rowLists = list()
+	rowSets = [set() for index in xrange(9)]
+	colSets = [set() for index in xrange(9)]
+	boardSets = [set() for index in xrange(9)]
+
+	rowSets[0].add(7)
+
+	print rowSets
 	
-	for i in range(size):
-		boxList = [1,2,3,4,5,6,7,8,9]
-		cellList = [1,2,3,4,5,6,7,8,9]
-		numList = [1,2,3,4,5,6,7,8,9]
-		
-		
+	# for i in range(size):
+	# 	boxList = [1,2,3,4,5,6,7,8,9]
+	# 	cellList = [1,2,3,4,5,6,7,8,9]
+	# 	numList = [1,2,3,4,5,6,7,8,9]
+
+boardGen(9, 2)
+
